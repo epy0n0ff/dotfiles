cda69eaca0d64e8ad4b4a3d6f5f5cb857d2a5fa0
epy0n0ff <epy0n.0ff@gmail.com>
2019-12-10 01:54:34 +0900

    initial commit

---
+++ etc/setup/01_apt.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo apt update
+sudo apt install peco
+sudo apt install tmux
+sudo apt install tmuxinator
+
---
+++ etc/setup/02_git.sh
@@ -0,0 +1,2 @@
+git config --global user.name epy0n0ff
+git config --global user.email epy0n.0ff@gmail.com
